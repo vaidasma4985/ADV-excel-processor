b74e725accec839cbbfb150ec4a178d11b44f920
vaidasma4985 <vaidas.ma4985@gmail.com>
2026-01-14 08:00:30 +0200

    Add wire sizing tool stub

---
+++ wire_tool/wire_app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+def render_wire_page() -> None:
+    st.subheader("Wire sizing tool")
+    st.info("Wire tool stub is working. Next step: upload + Power filter preview.")
