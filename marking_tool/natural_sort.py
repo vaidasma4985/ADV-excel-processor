60c958919ae3acc665658ab4e464e7448f1e2007
vaidasma4985 <vaidas.ma4985@gmail.com>
2026-06-17 14:18:51 +0300

    Clean up baigtas

--- marking_tool/natural_sort.py
+++
@@ -1,9 +0,0 @@
-from .shared.natural_sort import (
-    natural_marking_name_sort_key,
-    natural_marking_suffix_sort_key,
-)
-
-__all__ = [
-    "natural_marking_name_sort_key",
-    "natural_marking_suffix_sort_key",
-]
--- marking_tool/wire_processor.py
+++
@@ -1,3 +0,0 @@
-from .wire_cable_marking import build_wire_placeholder_result
-
-__all__ = ["build_wire_placeholder_result"]
